9dcd5b93b6591db626487d73b2243988b1dff40a
Caio <caio@teste.com>
2026-05-18 21:00:25 -0300

    Implements viewsets, URLs, and unit tests for product and order

---
+++ order/tests/test_viewsets.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APIClient
+from product.tests.factories import ProductFactory
+from order.models.order import Order
+
+@pytest.mark.django_db
+def test_create_order():
+    client = APIClient()
+    user = User.objects.create_user(username='johndoe', password='password')
+    product = ProductFactory()
+    
+    client.force_authenticate(user=user)
+    
+    url = reverse('order-list')
+    data = {
+        "user": user.id,
+        "product_id": [product.id]
+    }
+    
+    response = client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
